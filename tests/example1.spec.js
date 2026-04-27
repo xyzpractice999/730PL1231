404b441aa67ada1951de1c0ce64f5cbe380cc0ce
xyzpractice999 <skill2automate@gmail.com>
2026-04-27 08:08:38 +0530

    Remove duplicate title expectation in tests
    Removed duplicate title expectation check in example1.spec.js

--- tests/example1.spec.js
+++ tests/example1.spec.js
@@ -6,6 +6,9 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
