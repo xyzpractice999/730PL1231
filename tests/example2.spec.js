7551d35474833d73f6e76dff937f2b8da84f50f8
kishore <skill2automate@gmail.com>
2026-04-28 07:54:32 +0530

    conflit 1

--- tests/example2.spec.js
+++ tests/example2.spec.js
@@ -22,7 +22,7 @@ test('get started link 44', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Get started' }).selectOption('a');
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
